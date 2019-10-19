e7faec1301c6cfa7c15f7c5f985cdacc34adfc2b
SpaceShooters <56750992+SpaceShooters@users.noreply.github.com>
2019-10-19 11:24:02 -0600

    Add files via upload

--- flood-explorer-web/libs/graphicsComponent.js
+++ flood-explorer-web/libs/graphicsComponent.js
@@ -1,6 +1,10 @@
-    google.charts.load('current', {'packages':['corechart']});
+    google.charts.load('current', {'packages':['corechart','bar']});
     document.getElementById("load").style.display = "none";
     let endpoint = 'https://power.larc.nasa.gov/cgi-bin/v1/DataAccess.py?request=execute&identifier=SinglePoint&';    
+
+    function setParameters(initDate, endDate, lat, long, type){
+      return `parameters=${type}&startDate=${initDate.replace(/-/g,'')}&endDate=${endDate.replace(/-/g,'')}&userCommunity=SSE&tempAverage=DAILY&outputList=JSON,ASCII&lat=${lat}&lon=${long}&user=anonymous`;
+    }
 
     function getTemperature(){
       document.getElementById("load").style.display = "block";
@@ -12,28 +16,53 @@
         let endDate = document.getElementById("endDate").value;
         let lat = sessionStorage.getItem("lat");
         let long = sessionStorage.getItem("long");        
-        let path = `parameters=T2M&startDate=${initDate.replace(/-/g,'')}&endDate=${endDate.replace(/-/g,'')}&userCommunity=SSE&tempAverage=DAILY&outputList=JSON,ASCII&lat=${lat}&lon=${long}&user=anonymous`;
+        let path = setParameters(initDate, endDate, lat, long, 'T2M,TQV');
 
 
-        let temperature = await axios.get(`${endpoint}${path}`);
-        let dataProcess = process(temperature.data.features["0"].properties.parameter.T2M);
+        let charts = await axios.get(`${endpoint}${path}`);
+
+        //Chart Temperature
+        let dataProcess = process('Rango de fechas','℃',charts.data.features["0"].properties.parameter.T2M);
         var data = google.visualization.arrayToDataTable(dataProcess);
     
-      var options = {
-        title: 'Temperatura',
-        hAxis: {title: 'Fecha',  titleTextStyle: {color: '#333'}},
-        vAxis: {minValue: 0}
-      };
+        var options = {
+          title: `Estadísticas de temperatura en (${lat},${long})`,
+          hAxis: {title: 'Rango de fechas',  titleTextStyle: {color: '#333'}},
+          vAxis: {title: 'Temperatura en ℃'}
+        };
     
-      var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
-      document.getElementById("load").style.display = "none";              
-      chart.draw(data, options);
+        var chart = new google.visualization.AreaChart(document.getElementById('chart_temperature'));
+        chart.draw(data, options);
 
-    }
+        //Chart Precipitable water
+        dataProcess = process('Rango de fechas','Centímetros',charts.data.features["0"].properties.parameter.TQV);
+        data = google.visualization.arrayToDataTable(dataProcess);
+        options = {
+          trendlines: {
+            0: {type: 'linear', lineWidth: 5, opacity: .3},
+            1: {type: 'exponential', lineWidth: 10, opacity: .3}
+          },
+          title: `Estadísticas de agua precipitable en (${lat},${long})`,
+          hAxis: {title: 'Rango de fechas',  titleTextStyle: {color: '#333'},
+          viewWindow: {
+            min: [0,0],
+            max: [0, 0]
+          }
+          },
+          vAxis: {title: 'Medida en centímetros'}
+        };
+    
+        chart = new google.visualization.ColumnChart(document.getElementById('chart_precipitable'));
+        chart.draw(data, options);
 
-    function process(obj){
+
+        document.getElementById("load").style.display = "none";                      
+
+    }  
+
+    function process(x,y,obj){
         let array = [];
-        array.push(['Fecha','℃']);
+        array.push([x,y]);
 
         for(let o in obj){
             array.push([formatDate(o),obj[o]]);
@@ -45,4 +74,3 @@
     function formatDate(date){
       return date.substring(0,4)+'-'+date.substring(4,6)+'-'+date.substring(6,8);
     }
-
